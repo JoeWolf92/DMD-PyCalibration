2114754795c2ae524092f5a6eab67ab24f1abcd3
JoeWolf92 <17269559+JoeWolf92@user.noreply.github.com>
2020-01-17 13:34:35 +0000

    Added calibration functionality and tests

--- ShapeRecognition.py
+++ ShapeRecognition.py
@@ -1,39 +1,55 @@
 # import the necessary packages
 import cv2
+import numpy as np
 
 class ShapeDetector:
-	def __init__(self):
-		pass
+    def __init__(self, filename, shapeColour):
+        # Initiate image containing shapes to be identified
+        if shapeColour == "black":
+            self.shapeColour = 1
+        else:
+            self.shapeColour = 0
+        self.sourceImage = cv2.imread(filename,cv2.IMREAD_GRAYSCALE)
+        self.colourImage = cv2.cvtColor(self.sourceImage,cv2.COLOR_RGB2BGR)
+        if self.shapeColour == 1:
+            thresholdImageInitial = cv2.bitwise_not(self.sourceImage)
+            _, self.thresholdImage = cv2.threshold(thresholdImageInitial, 200, 255, cv2.THRESH_BINARY)
+        else:
+            _, thresholdImageInitial = cv2.threshold(self.sourceImage, 200, 255, cv2.THRESH_BINARY)
+            self.thresholdImage = thresholdImageInitial
+        openKernel = cv2.getStructuringElement(cv2.MORPH_RECT,(10,10))
+        self.filteredImage = cv2.morphologyEx(self.thresholdImage, cv2.MORPH_OPEN, openKernel)
+        closeKernel = cv2.getStructuringElement(cv2.MORPH_RECT,(15,15))
+        self.filteredImage = cv2.morphologyEx(self.filteredImage, cv2.MORPH_CLOSE, closeKernel)
+        self.rotation = 0.0
+        self.positionX = 0.0
+        self.positionY = 0.0
+        self.width = 0.0
+        self.height = 0.0
 
-	def detect(self, c):
-		# initialize the shape name and approximate the contour
-		shape = "unidentified"
-		peri = cv2.arcLength(c, True)
-		approx = cv2.approxPolyDP(c, 0.04 * peri, True)
+    def detectCalibration(self):
+        # Detect calibration from input image
+        contours, _ = cv2.findContours(self.filteredImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        shapeAreas = [cv2.contourArea(cntr) for cntr in contours]
+        maxShapeArea = np.max(shapeAreas)
+        #print('First Max: ' + str(maxShapeArea))
+        #shapeAreas.remove(maxShapeArea)
+        #maxShapeArea = np.max(shapeAreas)
+        #print('New Max: ' + str(maxShapeArea))
+        for cntr in contours:
+            if((( cv2.contourArea(cntr) > maxShapeArea * 0.1) and (cv2.contourArea(cntr) < maxShapeArea) and self.shapeColour == 1 ) or self.shapeColour == 0):
+                #print(cv2.contourArea(cntr))
+                rect = cv2.minAreaRect(cntr)
+                box = cv2.boxPoints(rect)
+                box = np.int0(box)
+                cv2.drawContours(self.colourImage,[box],0,(0,255,0),3)
+                centre = rect[0]
+                self.positionX = centre[0]
+                self.positionY = centre[1]
+                size = rect[1]
+                self.width = size[0]
+                self.height = size[1]
+                self.rotation = rect[2]
+                cv2.circle(self.colourImage,(round(self.positionX),round(self.positionY)), 5, (0,0,255), -1)
 
-        # if the shape is a triangle, it will have 3 vertices
-		if len(approx) == 3:
-			shape = "triangle"
- 
-		# if the shape has 4 vertices, it is either a square or
-		# a rectangle
-		elif len(approx) == 4:
-			# compute the bounding box of the contour and use the
-			# bounding box to compute the aspect ratio
-			(x, y, w, h) = cv2.boundingRect(approx)
-			ar = w / float(h)
- 
-			# a square will have an aspect ratio that is approximately
-			# equal to one, otherwise, the shape is a rectangle
-			shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
- 
-		# if the shape is a pentagon, it will have 5 vertices
-		elif len(approx) == 5:
-			shape = "pentagon"
- 
-		# otherwise, we assume the shape is a circle
-		else:
-			shape = "circle"
- 
-		# return the name of the shape
-		return shape
+
--- UnitTests.py
+++ UnitTests.py
@@ -1,8 +1,10 @@
 import sys
 import unittest
 from PyQt5 import QtCore, QtGui, QtWidgets, Qt, QtTest
+import cv2
 
 import appMainWindow
+import ShapeRecognition
 
 app = QtWidgets.QApplication(sys.argv)
 
@@ -12,13 +14,50 @@ class appMainWindowTests(unittest.TestCase):
         # Create the GUI
         self.form = appMainWindow.appMainWindow()
 
-    def test_defaults(self):
+    def test_Defaults(self):
         # Test default values of GUI contents
         self.assertEqual(self.form.ui.btn_CamImageImport.text(), "Import Camera Image")
-        self.assertEqual(self.form.ui.btn_DMDMaskGen.text(),"Generate DMD Mask")
-        self.assertEqual(self.form.ui.btn_DMDMaskSave.text(),"Save DMD Mask")
-        self.assertEqual(self.form.ui.btn_Calibrate,"Calibrate")
+        self.assertEqual(self.form.ui.btn_DMDMaskGen.text(), "Generate DMD Mask")
+        self.assertEqual(self.form.ui.btn_DMDMaskSave.text(), "Save Current Mask")
+        self.assertEqual(self.form.ui.btn_Calibrate.text(), "Calibrate")
+        self.assertEqual(self.form.ui.cbox_CalibrateCheck.text(), "Lock Calibration?")
+        self.assertEqual(self.form.ui.cbox_CalibrateCheck.isChecked(), False)
         
+    def test_ImageRecognition(self):
+        # Test basic funtionality of the shape recognition sub-system with two test images
+        # Image 1 is a black rectangle
+        testImage = ShapeRecognition.ShapeDetector("TestImages/BlackRectangle.png", "black")
+        #cv2.imshow("BlackRec",testImage.sourceImage)
+        #cv2.imshow("Threshold BlackRec", testImage.thresholdImage)
+        # cv2.imshow("BlackRecFiltered", cv2.resize(testImage.filteredImage, (960, 540)))
+        # cv2.waitKey(3500)
+        # cv2.destroyAllWindows()
+        testImage.detectCalibration()
+        self.assertAlmostEqual(testImage.rotation, -45.19929122924805)
+        self.assertAlmostEqual(testImage.width, 850.6590576171875)
+        self.assertAlmostEqual(testImage.height, 488.5807800292969)
+        self.assertAlmostEqual(testImage.positionX, 795.543212890625)
+        self.assertAlmostEqual(testImage.positionY, 551.9424438476562)
+        cv2.imshow("BlackRec with Contours", cv2.resize(testImage.colourImage,(960, 540)))
+        cv2.waitKey(3500)
+        cv2.destroyAllWindows()
+        # Image 2 is a white rectangle
+        testImage = ShapeRecognition.ShapeDetector("TestImages/WhiteRectangle.png", "white")
+        #cv2.imshow("WhiteRec",testImage.sourceImage)
+        #cv2.imshow("Threshold WhiteRec", testImage.thresholdImage)
+        # cv2.imshow("WhiteRecFiltered", cv2.resize(testImage.filteredImage, (960, 540)))
+        # cv2.waitKey(3500)
+        # cv2.destroyAllWindows()
+        testImage.detectCalibration()
+        self.assertAlmostEqual(testImage.rotation, -44.041961669921875)
+        self.assertAlmostEqual(testImage.width, 857.2245483398438)
+        self.assertAlmostEqual(testImage.height, 486.2369384765625)
+        self.assertAlmostEqual(testImage.positionX, 1146.5018310546875)
+        self.assertAlmostEqual(testImage.positionY, 561.4849853515625)
+        cv2.imshow("WhiteRec with Contours", cv2.resize(testImage.colourImage,(960, 540)))
+        cv2.waitKey(3500)
+        cv2.destroyAllWindows()
+
         
 if __name__ == '__main__':
     unittest.main()
